6ddec019c13595114435ee7b78b84168f698f920
Gon <zx1417@naver.com>
2026-05-08 14:21:16 +0900

    week6 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,22 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import ProtectedLayout from './components/ProtectedLayout';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import MyPage from './pages/MyPage';
-import GoogleCallbackPage from './pages/GoogleCallbackPage';
+import { useState } from 'react';
+import { UserDataDisplay } from './components/UserDataDisplay';
 
 function App() {
+  const [userId, setUserId] = useState<number>(1);
+
+  const handleRandomUser = () => {
+    const randomId = Math.floor(Math.random() * 15) + 1;
+    setUserId(randomId);
+  };
+
   return (
-    <BrowserRouter>
-      <nav style={{ padding: '20px', borderBottom: '1px solid black', marginBottom: '20px' }}>
-        <Link to="/" style={{ marginRight: '10px' }}>홈으로</Link>
-        <Link to="/login" style={{ marginRight: '10px' }}>로그인(Public)</Link>
-        <Link to="/my">마이페이지(Protected)</Link>
-      </nav>
-
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/v1/auth/google/callback" element={<GoogleCallbackPage />} />
-
-        <Route element={<ProtectedLayout />}>
-          <Route path="/my" element={<MyPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div style={{ padding: '20px' }}>
+      <button onClick={handleRandomUser} style={{ marginBottom: '20px' }}>
+        다른 사용자 불러오기 (현재 ID: {userId})
+      </button>
+      
+      <UserDataDisplay userId={userId} />
+    </div>
   );
 }
 
